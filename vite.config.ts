5cb8c1d3b49a557761861980ef1f30e702be04e8
AcaDjokic <djokicaleksandar32@gmail.com>
2025-05-07 01:31:56 +0200

    Podesavanje frontenda

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'KilexWebSite',
+  base: '/KilexWebSite',
   plugins: [react()],
   server: {
     proxy: {
