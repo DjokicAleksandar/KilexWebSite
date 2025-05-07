75faadc7cfc23a16e2626cf784e9198105e4ab57
AcaDjokic <djokicaleksandar32@gmail.com>
2025-05-07 02:02:33 +0200

    Cors origin podesen na backend

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 require("dotenv").config();
 
 const corsOptions = {
-  origin: "http://localhost:5173", // frontend
+  origin: "https://kilexwebsite.onrender.com", // frontend
   credentials: true,
 };
 
